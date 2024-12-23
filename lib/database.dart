c02eb93f823269bd3faedb911e77d2022c21fe33
Md Abdullah Al Siddik <119064797+mdabdullahalsiddik@users.noreply.github.com>
2024-12-23 14:54:20 +0600

    done

--- lib/database.dart
+++ lib/database.dart
@@ -1,6 +1,6 @@
 class DatabaseManager {
   static const String companyName = "Classic Advocate Office";
-  static const String companyMail = "info@classicit.com";
-  static const String companyPhone = '01737374083';
-  static const String companyWebSite = 'www.classicit.com';
+  static const String companyMail = "info@classicit.com.bd";
+  static const String companyPhone = '+8809678783783';
+  static const String companyWebSite = 'www.classicit.com.bd';
 }
